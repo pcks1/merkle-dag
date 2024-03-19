b2b06f7eac1c6bf6fd93f0d143f430f00e9d31cb
pcks <1924692207@qq.com>
2024-03-19 15:29:11 +0800

    dag2file.go

--- dag.go
+++ dag.go
@@ -6,29 +6,35 @@ import (
 	"hash"
 )
 
+// 定义常量
 const (
 	K          = 1 << 10
 	BLOCK_SIZE = 256 * K
 )
 
+// Link 结构体表示Merkle DAG中的链接
 type Link struct {
 	Name string
 	Hash []byte
 	Size int
 }
 
+// Object 结构体表示Merkle DAG中的对象
 type Object struct {
 	Links []Link
 	Data  [][]byte
 }
 
+// Stack 用于存储对象的栈
 var Stack = make([]Object, 0)
 
+// ListNode 结构体表示链表节点
 type ListNode struct {
 	Hash []byte
 	Next []byte
 }
 
+// Bytes 方法将ListNode序列化为字节切片
 func (n *ListNode) Bytes() []byte {
 	// 创建一个足够大的缓冲区来存储Hash和Next
 	buf := make([]byte, len(n.Hash)+len(n.Next)+16)
@@ -40,9 +46,12 @@ func (n *ListNode) Bytes() []byte {
 	copy(buf[16+len(n.Hash):], n.Next)
 	return buf
 }
+
+// Add 函数根据节点类型（文件或目录）将其添加到存储中，并更新栈顶的Object
 func Add(store KVStore, node Node, h hash.Hash) []byte {
 	switch node.Type() {
 	case FILE:
+		// 处理文件类型的节点
 		file := node.(File)
 		fileHash, data := StoreFile(store, file, h)
 		// 从栈顶取出Object对象
@@ -54,6 +63,7 @@ func Add(store KVStore, node Node, h hash.Hash) []byte {
 		})
 		obj.Data = append(obj.Data, data)
 	case DIR:
+		// 处理目录类型的节点
 		dir := node.(Dir)
 		// 创建一个新的Object对象并添加到栈顶
 		obj := Object{
@@ -84,19 +94,17 @@ func Add(store KVStore, node Node, h hash.Hash) []byte {
 	return nil
 }
 
+// StoreFile 函数处理文件的存储
 func StoreFile(store KVStore, node File, h hash.Hash) ([]byte, []byte) {
 	t := []byte("blob")
 	if node.Size() > BLOCK_SIZE {
 		t = []byte("list")
 	}
-	// 计算分片数量
 	n := (node.Size() + BLOCK_SIZE - 1) / BLOCK_SIZE
 	// 初始化链表头节点的hash为nil
 	var headHash []byte = nil
 	for i := 0; i < int(n); i++ {
-		// 读取分片数据
 		data := node.Bytes()[uint64(i*BLOCK_SIZE):uint64((i+1)*BLOCK_SIZE)]
-		// 计算分片的hash
 		h.Reset()
 		h.Write(data)
 		fileHash := h.Sum(nil)
@@ -116,7 +124,9 @@ func StoreFile(store KVStore, node File, h hash.Hash) ([]byte, []byte) {
 	return headHash, t
 }
 
+// StoreDir 函数处理目录的存储
 func StoreDir(store KVStore, dir Dir, h hash.Hash) ([]byte, []byte) {
+
 	t := []byte("tree")
 	tree := Object{
 		Links: make([]Link, 0),
@@ -127,6 +137,7 @@ func StoreDir(store KVStore, dir Dir, h hash.Hash) ([]byte, []byte) {
 		node := it.Node()
 		switch node.Type() {
 		case FILE:
+			// 处理文件类型的节点
 			file := node.(File)
 			fileHash, _ := StoreFile(store, file, h)
 			tree.Links = append(tree.Links, Link{
@@ -135,6 +146,7 @@ func StoreDir(store KVStore, dir Dir, h hash.Hash) ([]byte, []byte) {
 				Size: int(file.Size()),
 			})
 		case DIR:
+			// 处理目录类型的节点
 			subDir := node.(Dir)
 			subDirHash, _ := StoreDir(store, subDir, h)
 			tree.Links = append(tree.Links, Link{
--- dag2file.go
+++ dag2file.go
@@ -1,7 +1,48 @@
 package merkledag
 
-// Hash to file
-func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
-	return nil
+import (
+	"encoding/json"
+	"errors"
+)
+
+// Hash2File 根据 hash 和 path 从 KVStore 中读取并返回对应的文件内容。
+func Hash2File(store KVStore, hash []byte, path string) ([]byte, error) {
+	// 从 KVStore 中获取 Node 对象
+	nodeData, err := store.Get(hash)
+	if err != nil {
+		return nil, err
+	}
+
+	// 解析 Node 对象
+	var node Node
+	if err := json.Unmarshal(nodeData, &node); err != nil {
+		return nil, err
+	}
+
+	// 根据 Node 类型处理
+	switch node.(type) {
+	case File:
+		// 如果是文件类型，直接返回文件内容
+		file := node.(File)
+		if file.Name() == path {
+			return file.Bytes(), nil
+		}
+	case Dir:
+		// 如果是目录类型，遍历目录
+		dir := node.(Dir)
+		iterator := dir.It()
+		for iterator.Next() {
+			currentNode := iterator.Node()
+			// 如果路径匹配，且节点为文件类型，返回文件内容
+			if currentNode.Name() == path && currentNode.Type() == FILE {
+				file, ok := currentNode.(File)
+				if !ok {
+					return nil, errors.New("iterator node type assertion to File failed")
+				}
+				return file.Bytes(), nil
+			}
+		}
+	}
+
+	return nil, errors.New("file not found")
 }
